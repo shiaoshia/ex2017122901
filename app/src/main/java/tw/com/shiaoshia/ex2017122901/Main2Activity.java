4daf37e7301817b924c8f76f2287dcc3a42cb691
shiaoshia <34782716+shiaoshia@users.noreply.github.com>
2017-12-29 10:47:07 +0800

    分享文字與接收文字

--- app/src/main/java/tw/com/shiaoshia/ex2017122901/Main2Activity.java
+++ app/src/main/java/tw/com/shiaoshia/ex2017122901/Main2Activity.java
@@ -1,13 +1,22 @@
 package tw.com.shiaoshia.ex2017122901;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
+
+    TextView tv01;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        
+        tv01=findViewById(R.id.textView);
+        Intent it = getIntent();
+        String data = it.getStringExtra(Intent.EXTRA_TEXT);
+        tv01.setText(data);
     }
 }
